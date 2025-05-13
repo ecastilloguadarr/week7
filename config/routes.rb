df5f0772852f0e1b40e23c699163ffc6e236761f
Elizabeth Castillo <elizabeth.castillo@kellogg.northwestern.edu>
2025-05-13 16:32:13 +0000

    saved 1

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   resources "companies"
   # get("/companies", {:controller => "companies", :action => "index"})
+  # get("/companies/new", {:controller => "companies", :action => "new"})
+  # get("/companies:id", {:controller => "companies", :action => "show"})
+ 
   resources "contacts"
   # get("/contacts", {:controller => "contacts", :action => "index"})
 
